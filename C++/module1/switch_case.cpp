9e2a0eb49eac614e442ac91756365e913a224c6d
Hasan9955 <hasanbinali7556@gmail.com>
2026-02-17 15:26:37 +0600

    all

---
+++ C++/module1/switch_case.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+int main () 
+{
+    int x;
+    cin >> x;
+
+    switch (x)
+    {
+    case 1:
+        cout << "x is 1" << endl;
+        break;
+    case 2:
+        cout << "x is 2" << endl;
+        break;    
+    case 3:
+        cout << "x is 3" << endl;
+        break;
+    case 4:
+        cout << "x is 4" << endl;
+        break;
+    case 5:
+        cout << "x is 5" << endl;
+        break;
+    case 6:
+        cout << "x is 6" << endl;
+        break;
+    case 7:
+        cout << "x is 7" << endl;
+        break;
+    case 8:
+        cout << "x is 8" << endl;
+        break;
+    case 9:
+        cout << "x is 9" << endl;
+        break;
+    case 10:
+        cout << "x is 10" << endl;
+        break;
+    
+    default:
+        cout << "x is not 1 to 10" << endl;
+        break;
+    }
+
+
+    return 0;
+}
