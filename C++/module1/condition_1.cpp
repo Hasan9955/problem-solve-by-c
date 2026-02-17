e1fcfdc4799051b31f10828437834b8162459f39
Hasan9955 <hasanbinali7556@gmail.com>
2026-02-17 15:19:10 +0600

    up

---
+++ C++/module1/condition_1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int x;
+
+    cin >> x;
+
+    // if(x == 10){
+    //     cout << "x is 10" << endl;
+    // }
+    // else{
+    //     cout << "x is not 10" << endl;
+    // }
+
+    // Ternary operator
+    cout << (x == 10 ? "x is 10" : "x is not 10") << endl;
+
+    return 0;
+}
