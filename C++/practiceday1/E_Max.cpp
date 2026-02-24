c402f673dff14b2e350a6a3101b0b94da5c1caed
Hasan9955 <hasanbinali7556@gmail.com>
2026-02-24 16:00:13 +0600

    update

---
+++ C++/practiceday1/E_Max.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+
+    int n;
+    
+    cin >> n;
+
+    long long int arr[n]; 
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    long long int max = arr[0];
+
+    for(int i = 0; i < n; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+
+    cout << max << endl;
+
+    return 0;
+
+
+}
