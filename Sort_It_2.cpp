541a4228939b6b540d0933125690bb79231611fd
Hasan9955 <hasanbinali7556@gmail.com>
2026-03-09 14:33:22 +0600

    second prob

--- Sort_It_2.cpp
+++ Sort_It_2.cpp
@@ -25,3 +25,38 @@
 
 
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int * sort_it (int n) {
+
+    
+    int *a = new int [n];
+    
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    
+    sort(a, a + n, greater<int>());
+
+    return a;
+}
+
+int main () {
+
+    int n;
+    cin >> n;
+
+
+
+    int *a = sort_it(n);
+
+    for(int i = 0; i < n; i++){
+        cout << a[i];
+        if(i < n - 1) cout << " ";
+    }
+    cout << endl;
+
+    delete[] a;
+    return 0;
+}
