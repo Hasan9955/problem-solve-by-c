399f0b45b651056cf3e114b504d1c3499ade45b1
Hasan9955 <hasanbinali7556@gmail.com>
2026-03-10 12:23:16 +0600

    mid exam

--- Choose_Three.cpp
+++ Choose_Three.cpp
@@ -41,3 +41,123 @@
 // In the first test case, we can make 10 by adding 5+4+1. There are other ways too.
 // In the second test case, it is not possible to make 6 by adding three different indexed values from the array.
 // In the third case, it is possible to make 6 by using three different indexed values.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, sum;
+
+        cin >> n >> sum;
+
+        // int arr[n];
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        sort(arr.begin(), arr.end());
+
+        bool isFound = false;
+
+
+        for (int i = 0; i < n - 2 && !isFound; i++)
+        {
+            int target = sum - arr[i];
+
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right)
+            {
+                int currentSum = arr[left] + arr[right];
+
+                if(currentSum == target){
+                    isFound = true;
+                    break;
+                } else if (currentSum < target){
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+            
+        }
+
+
+        cout << (isFound ? "YES" : "NO") << endl;
+
+        // OLDER VERSION
+        // int flag = 0;
+        // // for (int i = 0; i < n; i++)
+        // // {
+
+        // //     for (int j = 0; j < n; j++)
+        // //     {
+
+        // //         if (j == i)
+        // //         {
+        // //             continue;
+        // //         }
+
+        // //         for (int k = 0; k < n; k++)
+        // //         {
+        // //             if (k == i || k == j)
+        // //                 continue;
+
+        // //             if (arr[i] + arr[j] + arr[k] == sum)
+        // //             {
+        // //                 flag = 1;
+        // //                 break;
+        // //             }
+        // //         }
+        // //         if (flag)
+        // //             break;
+        // //     }
+        // //     if (flag)
+        // //         break;
+        // // }
+
+        // for (int i = 0; i < n; i++)
+        // {
+        //     for (int j = i + 1; j < n; j++)
+        //     {
+        //         for (int k = j + 1; k < n; k++)
+        //         {
+        //             if (arr[i] + arr[j] + arr[k] == sum)
+        //             {
+        //                 flag = 1;
+        //                 break;
+        //             }
+        //         }
+        //         if (flag)
+        //             break;
+        //     }
+        //     if (flag)
+        //         break;
+        // }
+
+        // if (flag == 0)
+        // {
+        //     cout << "NO";
+        // }
+        // else
+        // {
+        //     cout << "YES";
+        // }
+
+        // cout << endl;
+    }
+
+    return 0;
+}
