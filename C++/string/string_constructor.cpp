24cff10f230b8cf90801be228fcfb77077f20a06
Hasan9955 <hasanbinali7556@gmail.com>
2026-03-25 15:17:01 +0600

    string constructor

---
+++ C++/string/string_constructor.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+
+    // --------- option-1 ---------
+    // string s = "Hello world";
+
+    // --------- option-2 ---------
+    // string s("Hello World");
+
+    // --------- option-3 ---------
+    // string s("Hello World", 7);
+
+    // --------- option-4 ---------
+    // string a = "Hello World";
+    // string s(a, 4);
+
+
+    // --------- option-5 ---------
+    string s(26, 'a');
+
+
+    cout << s << endl;
+    return 0;
+}
