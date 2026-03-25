c631f4be2de413ed2a309a6add485dbd26837b3b
Hasan9955 <hasanbinali7556@gmail.com>
2026-03-25 10:42:33 +0600

    capacity

---
+++ C++/string/capacity.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+
+    // ----------- size() ----------------
+    // string s = "Hello world";
+    // cout << s.size() << endl;
+    // cout << s.max_size() << endl;
+
+
+
+    // -------------- capacity() -------------------
+    // c++ can increase capacity dynamically 
+    // string a = "jkljjdklsfjklsdfkljasklfjkldajklfjdsklfjdsklnncm,nvm,nxkl;jksladfjlaksdfj";
+    // cout << a.capacity() << endl;
+
+    // a = "jkljjdklsfjklsdfkljasklfjkldajklfjdsklfjdsklnncm,nvm,nxkl;jksladfjlaksdfjjkljjdklsfjklsdfkljasklfjkldajklfjdsklfjdsklnncm,nvm,nxkl;jksladfjlaksdfj";
+    
+    // cout << a.capacity() << endl;
+
+
+
+
+    // ---------- clear() --------------
+    // string b = "Hello";
+
+    // cout << b << endl;
+
+    // b.clear();
+
+    // cout << b << endl;
+
+
+
+
+    // ---------- empty() -------------------
+    // string c = "Hello";
+
+    // if(c.empty() == true) cout << "Empty" << endl;
+    // else cout << "Not Empty" << endl;
+
+
+
+    // -------------- resize() ----------------
+    // string s;
+    // cin >> s;
+    // s.resize(4);
+    // s.resize(8, 'X');
+    // cout << s.size() << endl;
+    // cout << s << endl;
+
+
+    return 0;
+}
