45106baa8bc08e68f4ab677fbf5de7bb9ce99196
Hasan9955 <hasanbinali7556@gmail.com>
2026-03-25 11:43:56 +0600

    string with space

---
+++ C++/string/string_input_with_space.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+
+    string s;
+ 
+    int n;
+
+    cin >> n;
+
+    // Those function are used to ignore the enter 
+    cin.ignore();
+    // getchar();
+
+    getline(cin, s);
+
+    cout << n << endl;
+    cout << s << endl;
+
+    cout << s.size() << endl;
+
+
+    return 0;
+}
