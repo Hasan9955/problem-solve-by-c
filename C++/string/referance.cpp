5d68ccb04b668cfd1be165e7b96027d0641253bd
Hasan9955 <hasanbinali7556@gmail.com>
2026-03-25 16:39:49 +0600

    reference

---
+++ C++/string/referance.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void print (string & s) {
+    
+    s = "Hasan";
+
+    cout << s << endl;
+
+}
+int main () {
+
+    string s = "Hello world";
+
+    print(s);
+
+
+    cout << s << endl;
+
+
+    return 0;
+}
