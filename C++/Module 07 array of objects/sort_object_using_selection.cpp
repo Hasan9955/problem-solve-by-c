d3b6867254de60360e948641284e21d7db988339
Hasan9955 <hasanbinali7556@gmail.com>
2026-03-28 11:16:23 +0600

    sort object

---
+++ C++/Module 07 array of objects/sort_object_using_selection.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Student 
+{
+    public:
+    string name;
+    int roll;
+    int mark;
+
+
+};
+
+
+int main () {
+
+    int n; 
+    cin >> n;
+    Student arr[n];
+
+    for(int i = 0; i < n; i++){
+        
+        cin >> arr[i].name >> arr[i].roll >> arr[i].mark;
+    }
+
+
+    for(int i = 0; i < n; i++){
+
+        for(int j = i; j < n; j++){
+            if(arr[i].mark < arr[j].mark){
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+
+
+    for(int i = 0; i<n; i++){
+        cout << arr[i].name << " " << arr[i].roll << " " << arr[i].mark << endl;
+    }
+
+
+    
+    return 0;
+}
