a3b9cef5d529afdc9ed2c82c261a920f6d9b10bc
Hasan9955 <hasanbinali7556@gmail.com>
2026-03-29 15:42:44 +0600

    sort

---
+++ Sort_It.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int cls;
+    char sec;
+    int roll;
+    int math_mark;
+    int eng_mark;
+};
+
+bool cmp (Student a, Student b) {
+    if(a.math_mark + a.eng_mark == b.math_mark + b.eng_mark){
+        return a.roll < b.roll;
+    } else return a.math_mark + a.eng_mark > b.math_mark + b.eng_mark;
+}
+
+
+int main()
+{
+
+    int n;
+
+    cin >> n;
+
+    Student arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i].name >> arr[i].cls >> arr[i].sec >> arr[i].roll >> arr[i].math_mark >> arr[i].eng_mark;
+    }
+
+
+    sort(arr, arr + n, cmp);
+
+
+    for(int i = 0; i < n; i++){
+        cout << arr[i].name << " " << arr[i].cls << " " << arr[i].sec << " " << arr[i].roll << " " << arr[i].math_mark << " " << arr[i].eng_mark << endl;
+    }
+
+}
