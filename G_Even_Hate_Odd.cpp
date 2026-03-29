5a3a31585a9930a147939d706e9c3da98863e616
Hasan9955 <hasanbinali7556@gmail.com>
2026-03-29 12:31:47 +0600

    even hate

--- G_Even_Hate_Odd.cpp
+++ G_Even_Hate_Odd.cpp
@@ -44,3 +44,47 @@
 
 
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+
+    int t;
+
+    cin >> t;
+
+    while (t--){
+        int n;
+        cin >> n;
+        
+        int arr[n];
+
+        for(int i = 0; i < n; i++){
+            cin >> arr[i];
+        }
+
+        if(n % 2 != 0){
+            cout << -1 << endl;
+        } else {
+            
+            int evenCount = 0;
+            int oddCount = 0;
+
+            for(int i = 0; i < n; i++){
+                if(arr[i] % 2 == 0){
+                    evenCount++;
+                } else {
+                    oddCount++;
+                }
+            }
+
+            if(evenCount == oddCount){
+                cout << 0 << endl;
+            } else {
+                cout << abs(evenCount - oddCount) / 2 << endl;
+            }
+
+        }
+    }
+    return 0;
+}
