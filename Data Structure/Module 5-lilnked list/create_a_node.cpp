fe0ccac63bf3ed62079162fc93f9fbe2b25d6918
Hasan9955 <hasanbinali7556@gmail.com>
2026-04-02 11:27:38 +0600

    update node

--- Data Structure/Module 5-lilnked list/create_a_node.cpp
+++ Data Structure/Module 5-lilnked list/create_a_node.cpp
@@ -7,18 +7,27 @@ class Node
     public:
      int val;
      Node* next;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
 };
 
 
 int main () {
 
-    Node a, b;
-
-    a.val = 10;
-    b.val = 20;
+    Node a(10), b(20);
 
     a.next = &b;
-    b.next = NULL;
+    
+
+
+    // a.val = 10;
+    // b.val = 20;
+
+    // a.next = &b;
+    // b.next = NULL;
 
     cout << a.val << endl;
 
