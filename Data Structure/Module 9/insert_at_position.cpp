d7145f0077d0092aecc659cf78e8f0157b9c781f
Hasan9955 <hasanbinali7556@gmail.com>
2026-04-19 11:35:06 +0600

    insert at position

---
+++ Data Structure/Module 9/insert_at_position.cpp
@@ -0,0 +1,136 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_normal(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+
+    cout << endl;
+}
+
+void print_reverse(Node *tail)
+{
+    Node *temp = tail;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->prev;
+    }
+
+    cout << endl;
+}
+
+void insert_at_tail(Node *&head, Node *&tail, int v)
+{
+
+    Node *newNode = new Node(v);
+
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+
+int size (Node *head){
+    int count = 0;
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        count++;
+        temp = temp->next;
+    }
+
+    return count;
+    
+}
+
+void insert_at_pos(Node *&head, Node *&tail, int pos, int v)
+{
+
+    Node *newNode = new Node(v);
+    if (pos == 0)
+    {
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+        return;
+    }
+
+    Node *temp = head;
+
+    for (int i = 1; i < pos - 1; i++)
+    {
+        temp = temp->next;
+    }
+
+    cout << temp->val << endl;
+
+    newNode->next = temp->next;
+    newNode->prev = temp;
+    temp->next->prev = newNode;
+    temp->next = newNode;
+}
+
+int main()
+{
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while (true)
+    {
+        int v;
+        cin >> v;
+
+        if (v == -1)
+        {
+            print_normal(head);
+            print_reverse(tail);
+            break;
+        }
+
+        insert_at_tail(head, tail, v);
+    }
+
+    while (true)
+    {
+        int pos, v;
+        cin >> pos >> v;
+
+        insert_at_pos(head, tail, pos, v);
+
+        print_normal(head);
+        print_reverse(tail);
+    }
+
+    return 0;
+}
