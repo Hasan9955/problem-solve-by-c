4fce3c0f7a845cd7ac584a615f08bbd6e61eccb4
Hasan9955 <hasanbinali7556@gmail.com>
2026-04-19 08:41:37 +0600

    remove duplicate

---
+++ Remove_Duplicate.cpp
@@ -0,0 +1,105 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Node 
+{
+    public:
+     int val;
+     Node* next;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+
+void insert_at_tail (Node *&head, Node *&tail, int v){
+    Node *newNode = new Node(v);
+
+    if(head == NULL){
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    tail = newNode;
+
+}
+
+void print_linked_list(Node *head){
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+
+    cout << endl;
+    
+}
+
+int size(Node *head){
+    int count = 0;
+    Node *temp = head;
+
+    while (temp != 0)
+    {
+        count++;
+        temp = temp->next;
+    }
+    
+    return count;
+}
+
+
+void remove_duplicates(Node *head){
+    Node *i = head;
+
+    while (i != NULL){
+        Node *prev = i;
+        Node *j = i->next;
+
+        while (j != NULL)
+        {
+            if(i->val == j->val){
+                prev->next = j->next;
+                delete j;
+                j = prev->next;
+            } else {
+                prev = j;
+                j = j->next;
+            }
+        }
+
+        i = i->next;
+        
+    }
+
+}
+
+
+int main () {
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while (true)
+    {
+        int v;
+        cin >> v;
+
+        if(v == -1){
+            remove_duplicates(head);
+            print_linked_list(head);
+            break;
+        }
+
+        insert_at_tail(head, tail, v);
+    }
+    
+    return 0;
+}
