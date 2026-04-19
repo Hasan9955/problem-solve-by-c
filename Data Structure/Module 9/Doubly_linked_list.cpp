8d9f7e2a5e27387ee3d0315b7ea41ed18741cb97
Hasan9955 <hasanbinali7556@gmail.com>
2026-04-19 09:43:16 +0600

    doubly_linked_list

---
+++ Data Structure/Module 9/Doubly_linked_list.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Node 
+{
+    public:
+     int val;
+     Node* next;
+     Node* prev;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+
+void print_normal(Node * head){
+    Node * temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+
+    cout << endl;
+}
+
+void print_reverse(Node * tail) {
+    Node *temp = tail; 
+
+
+    while (temp != NULL){
+        cout << temp->val << " ";
+        temp = temp->prev;
+    }
+
+    cout << endl;
+}
+
+
+
+int main () {
+
+    Node * head = new Node(10);
+    Node * a = new Node(20);
+    Node * b = new Node(30);
+    Node *tail = new Node(40);
+    
+
+    // connection
+    head->next = a;
+    a->prev = head;
+    a->next = b;
+    b->prev = a;
+    b->next = tail;
+    tail->prev = b;
+
+    print_normal(head);
+    print_reverse(tail);
+    return 0;
+}
