3eb6e3f44e7de08cbbd2f5b45bd2f2f2ae36a6d6
Hasan9955 <hasanbinali7556@gmail.com>
2026-04-22 12:35:51 +0600

    reverse doubly linked list

---
+++ Data Structure/Module 10/reverse_doubly_linked_list.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Node 
+{
+    public:
+     int val;
+     Node* next;
+     Node * prev;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+
+void print_normal(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+
+    cout << endl;
+}
+
+
+void insert_at_tail(Node *&head, Node *&tail, int v)
+{
+
+    Node *newNode = new Node(v);
+
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+
+ 
+void reverse(Node *&head, Node *tail){
+    Node *i = head;
+    Node *j = tail;
+
+    while (i != j && i->next != j)
+    {
+        swap(i->val, j->val);
+        i = i->next;
+        j = j->prev;
+    }
+    
+}
+
+
+int main () {
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while (true)
+    {
+        int v;
+        cin >> v;
+
+        if (v == -1)
+        {
+            reverse(head, tail);
+            print_normal(head); 
+            break;
+        }
+
+        insert_at_tail(head, tail, v);
+    }
+ 
+    
+    return 0;
+}
