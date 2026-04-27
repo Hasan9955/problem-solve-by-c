a0f41165e33cbba98f47f31156749391b680135d
Hasan9955 <hasanbinali7556@gmail.com>
2026-04-27 12:25:41 +0600

    stack using stl

---
+++ Data Structure/module 13 stack/stl_stack.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    stack<int> st;
+
+    st.push(10);
+    st.push(20);
+    st.push(30);
+
+    while (!st.empty())
+    {
+        cout << st.top() << endl;
+        st.pop();
+    }
+
+    return 0;
+}
