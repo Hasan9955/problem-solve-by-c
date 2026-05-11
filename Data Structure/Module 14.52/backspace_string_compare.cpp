5b34824aa566983c033df17bbe31cf9d0dc124ea
Hasan Bin Ali <hasanbinali7556@gmail.com>
2026-05-11 22:29:59 +0600

    backspace string compare

---
+++ Data Structure/Module 14.52/backspace_string_compare.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main () {
+    
+    string s1, s2;
+    cin >> s1 >> s2;
+
+
+    for(int i = 0; i < s1.size(); i++){
+        if(s1[i] == '#'){
+            s1.erase(i-1, 2);
+            i = i - 2;
+        }
+    }
+
+    for(int i = 0; i < s2.size(); i++){
+        if(s2[i] == '#'){
+            s2.erase(i-1, 2);
+            i = i - 2;
+        }
+    }
+
+    if(s1 == s2) cout << "YES" << endl;
+    else cout << "NO" << endl;
+    return 0;
+}
