a37e0a25b67def2979fc7d5225a7807ad5b659ed
Hasan Bin Ali <hasanbinali7556@gmail.com>
2026-06-06 15:18:43 +0600

    module 2 start

---
+++ python/week 1/module 2/default_args_kargs.py
@@ -0,0 +1,7 @@
+
+
+def all_sum(*number):
+    print(number)
+    return number
+
+print("all num: ", all_sum(50, 20, 40))
--- python/week 1/module 2/function.py
+++ python/week 1/module 2/function.py
@@ -4,7 +4,7 @@ def sum(num1, num2):
     total = num1 + num2
     return total
 
-num1 = 100
-num2 = 200
+num1 = int(input())
+num2 = int(input())
 total = sum(num1, num2)
 print(total)
