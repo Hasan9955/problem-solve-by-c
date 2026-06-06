5ef1e450e636d06bfb2117c1b6f41539b9d3acb2
Hasan Bin Ali <hasanbinali7556@gmail.com>
2026-06-06 20:31:11 +0600

    array in python

---
+++ python/week 1/module 2/array.py
@@ -0,0 +1,16 @@
+# list, array, collection is same 
+
+# index     0  1  2  3  4
+numbers = [ 2, 4, 5, 7, 8 ]
+#rev ind:  -5 -4 -3 -2 -1
+
+
+print(numbers[3], numbers[-4])
+
+print(numbers[1:4])
+
+print(numbers[1:4:1])
+print(numbers[0:7:2])
+print(numbers[-1:-7:-2])
+print(numbers[1:])
+print(numbers[::-1])
