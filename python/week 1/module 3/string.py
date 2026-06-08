5a8faf5dfa59cdc8b09a991566e1ef06279f026c
Hasan Bin Ali <hasanbinali7556@gmail.com>
2026-06-08 22:35:32 +0600

    string module

---
+++ python/week 1/module 3/string.py
@@ -0,0 +1,25 @@
+
+
+name = "hasan bin ali"
+name2 = 'Hasan\'s shop'
+name3 = """
+    Hasan 
+    Bin 
+    Ali
+"""
+
+# print(name)
+# print(name2)
+# print(name3)
+
+# for ch in name:
+#     print(ch)
+
+
+print(name[3])
+print(name[1:6])
+print(name[-3])
+# reversed string 
+print(name[::-1])
+print(name.upper())
+print(name.count('a'))
