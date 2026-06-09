fd96e582d9ab1f4332d1389542e1891b80fa7e43
Hasan Bin Ali <hasanbinali7556@gmail.com>
2026-06-09 20:23:05 +0600

    build in module

---
+++ python/week 1/module 3/build_in_module.py
@@ -0,0 +1,19 @@
+from math import *
+from random import *
+from time import sleep
+
+# result = floor(4.6)
+result = ceil(4.6)
+print(result)
+
+print(random())
+print(randint(1, 100))
+print(choice(['Hasan', 'Anas', 'Tahura', 'Tanjina', 'Abdullah']))
+deck =  ['Hasan', 'Anas', 'Tahura', 'Tanjina', 'Abdullah']
+shuffle(deck)
+
+# sleeping for 4 second
+sleep(4)
+print(deck)
+
+
