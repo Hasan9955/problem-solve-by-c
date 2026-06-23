90a3472e7e61b4ba18d3cdb365266b716622a464
Hasan Bin Ali <hasanbinali7556@gmail.com>
2026-06-23 22:26:16 +0600

    hello py

--- python/week 2/module 7 oop more/singleton.py
+++ python/week 2/module 7 oop more/singleton.py
@@ -26,3 +26,40 @@ print(second)
 print(third)
 
 # last = Singleton()
+
+
+
+'''
+
+map $http_upgrade $connection_upgrade {
+default upgrade;
+'' close;
+}
+
+server {
+    listen 80;
+    server_name api.sellapy.com;
+    
+    client_max_body_size 100M;
+
+    location / {
+        proxy_pass http://localhost:8010;
+
+        # HTTP headers
+        proxy_http_version 1.1;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+        # WebSocket support
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+
+        # SSE support
+        proxy_buffering off;
+        proxy_cache off;
+        proxy_set_header Cache-Control no-cache;
+    }
+}
+
+'''
